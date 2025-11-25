ab8a30ebab06aa5aa327947f4f568d60e5cd1474
Trey Trent <Trey.Trent@Insight.com>
2025-11-25 11:20:50 -0700

    chore(typescript-playground): ip-3 - implements toTitleCase()

--- pocs/typescript-playground/src/strings/strings.ts
+++ pocs/typescript-playground/src/strings/strings.ts
@@ -1,3 +1,16 @@
 export function updateTarget(input: string, target: string, update: string): string {
     return input.replace(target, update)
 }
+
+export function toTitleCase(input: string): string {
+    if (input != "") {
+        let str2 = input.charAt(0).toUpperCase() + input.slice(1) // Capitalize first letter
+        for (let i = 1; i < str2.length; ++i) { 
+            if (str2.charAt(i) == " ") { // Find first whitespace char
+                str2 = str2.slice(0, i + 1) + str2.charAt(i + 1).toUpperCase() + str2.slice(i + 2) // Slice existing, capitalize char after whitespace, add rest of string
+            }
+        }
+        return str2
+    }
+    return input
+}
