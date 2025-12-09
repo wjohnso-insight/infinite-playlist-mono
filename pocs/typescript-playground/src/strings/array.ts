da5faa227e1322ddc3b8b21d3d79f4a50d7d5e5c
Trey Trent <Trey.Trent@Insight.com>
2025-12-09 11:01:15 -0700

    Added proper spacing for readability

--- pocs/typescript-playground/src/strings/array.ts
+++ pocs/typescript-playground/src/strings/array.ts
@@ -1,18 +1,22 @@
 export function unique<T>(arr: T[]): T[] {
     const newArr: T[] = []
     const seenKeys: string[] = []
+
     for (const item of arr) {
         let key: string
         const valueType = typeof item 
+        
         if (valueType == "object") { 
             key = `${JSON.stringify(item)}`
         } else {
             key = `${String(item)}`
         }
+
         if (!seenKeys.includes(key)) { 
             seenKeys.push(key)
             newArr.push(item)
         }
+
     }
     return newArr
 }
