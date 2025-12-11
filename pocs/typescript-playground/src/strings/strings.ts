6b3d48290b0c054d7744c5ed8f1f95d43fb9213b
trtrent <Trey.Trent@Insight.com>
2025-12-11 15:27:46 -0700

    chore(typescript-playground): ip-4 - implements isPalindrome() (#10)
    * chore(typescript-playground): ip-3 - implements toTitleCase()
    
    * chore(typescript-playground): ip-4 - implements isPalindrome()
    
    * Fix linting error
    
    * Improved spacing, removed unnecessary comments

--- pocs/typescript-playground/src/strings/strings.ts
+++ pocs/typescript-playground/src/strings/strings.ts
@@ -4,16 +4,29 @@ export function updateTarget(input: string, target: string, update: string): str
 
 export function toTitleCase(input: string): string {
     if (input != "") {
-        let str2 = input.charAt(0).toUpperCase() + input.slice(1)
-        
+        let str2 = input.charAt(0).toUpperCase() + input.slice(1) // Capitalize first letter
         for (let i = 1; i < str2.length; ++i) { 
-            if (str2.charAt(i) == " ") {
-                // Slice existing, capitalize char after whitespace, add rest of string
-                str2 = str2.slice(0, i + 1) + str2.charAt(i + 1).toUpperCase() + str2.slice(i + 2)
+            if (str2.charAt(i) == " ") { // Find first whitespace char
+                str2 = str2.slice(0, i + 1) + str2.charAt(i + 1).toUpperCase() + str2.slice(i + 2) // Slice existing, capitalize char after whitespace, add rest of string
             }
         }
         return str2
     }
-
     return input
+}
+
+export function isPalindrome(input: string): boolean {
+    input = input.toLowerCase().replace(/[^a-z]/gi, "") // .replace to remove any chars that are not a letter
+    const j = input.length - 1;
+
+    if (input != "") {
+        for (let i = 0; i < input.length; ++i) {
+            if (input[i] != input[j - i]) {
+                return false
+            }
+        }
+        return true
+    }
+
+    return true
 }
