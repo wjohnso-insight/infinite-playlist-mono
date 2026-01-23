28fab5a5beccfb874ac1f047dbf502308db012b1
Trey Trent <Trey.Trent@Insight.com>
2026-01-23 09:19:30 -0700

    fix(express playground): moved buildhealthcheck from healthcheck.ts to well-known.ts

--- pocs/express-playground/src/controllers/v1/well-known.ts
+++ pocs/express-playground/src/controllers/v1/well-known.ts
@@ -1,7 +1,15 @@
-import { buildHealthCheck } from "../../models/healthcheck";
+import { IHealthcheck } from "../../models/healthcheck";
 import { Request, Response } from 'express'
+import packageJson from '../../../package.json'
 
 export const healthcheckController = (req: Request, res: Response) => {
+    const buildHealthCheck = () : IHealthcheck => {
+        const status = 'ok'
+        const version = packageJson.version
+        const timestamp = new Date().toISOString()
+        return { status, version, timestamp}
+    }
+
     const payload = buildHealthCheck()
     return res.status(200).json(payload)
 }
