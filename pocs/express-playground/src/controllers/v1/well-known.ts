294de44e1f91fff691cb33eddf0297f6dbd07d4f
Trey Trent <Trey.Trent@Insight.com>
2026-01-28 13:06:48 -0700

    fix(express-playground): status to be StatusEnum, created DefaultResponse

--- pocs/express-playground/src/controllers/v1/well-known.ts
+++ pocs/express-playground/src/controllers/v1/well-known.ts
@@ -1,15 +1,14 @@
-import { IHealthcheck } from "../../models/healthcheck";
+import { HealthcheckResponse } from "../../models/healthcheck";
 import { Request, Response } from 'express'
 import packageJson from '../../../package.json'
+import { HttpStatus } from "../../models/Http";
 
 export const healthcheckController = (req: Request, res: Response) => {
-    const buildHealthCheck = () : IHealthcheck => {
-        const status = 'ok'
-        const version = packageJson.version
-        const timestamp = new Date().toISOString()
-        return { status, version, timestamp}
+    const payload: HealthcheckResponse = {
+        status: HttpStatus.OK,
+        version: packageJson.version,
+        timestamp: new Date().toISOString()
     }
 
-    const payload = buildHealthCheck()
     return res.status(200).json(payload)
 }
--- pocs/express-playground/src/models/healthcheck.ts
+++ pocs/express-playground/src/models/healthcheck.ts
@@ -1,5 +1,8 @@
-export interface IHealthcheck {
-    status: string
+import { HttpStatus } from "./Http"
+import { DefaultResponse } from "./Http"
+
+export type HealthcheckResponse = DefaultResponse & {
+    status: HttpStatus
     version: string
     timestamp: string
 }
