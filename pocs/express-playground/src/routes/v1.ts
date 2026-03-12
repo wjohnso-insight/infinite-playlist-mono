519d7ba8c70fcbdd0e76b7ef26c453f07e505c47
Trey Trent <Trey.Trent@Insight.com>
2026-03-12 15:39:33 -0700

    chore(express-playground): milestone1 - authentication & user data

--- pocs/express-playground/src/routes/v1.ts
+++ pocs/express-playground/src/routes/v1.ts
@@ -1,6 +1,12 @@
 import { Router } from 'express';
 import { healthcheckController } from '../controllers/v1/well-known';
+import { spotifyCallbackController } from '../controllers/v1/spotifyController';
+import { spotifyLoginController } from '../controllers/v1/spotifyController';
 
 export const v1Router = Router()
 
 v1Router.get('/healthcheck', healthcheckController)
+
+v1Router.get('/auth/spotify/login', spotifyLoginController)
+
+v1Router.get('/auth/spotify/callback', spotifyCallbackController)
